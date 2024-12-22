086200e492bedfce4db7d6bfb642656b9571e4d9
andre-gnandt <gnandtandre@outlook.com>
2024-12-21 22:13:20 -0800

    Create controller and endpoint to get country region json data

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using LocalTreeData.Models;
 using LocalTreeData.Dtos;
 using LocalTreeData.Application;
-using System.Net;
-using System.Text;
 
 namespace LocalTreeData.Controllers
 {
