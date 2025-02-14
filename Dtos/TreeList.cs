ae231d4f2b524cd0aa746500a217ea8a3629acdc
andre-gnandt <gnandtandre@outlook.com>
2025-02-14 01:05:14 -0800

    Add conditional pagination to backend for only very large data sets

---
+++ Dtos/TreeList.cs
@@ -0,0 +1,9 @@
+namespace LocalTreeData.Dtos
+{
+    public class TreeList
+    {
+        public int Count { get; set; }
+        public int SearchCount { get; set; }
+        public List<TreeDto> Trees { get; set; }
+    }
+}
